90a61e5c30ca267a5515baeaf0a2eecffe6189f9
vkardaras <vasiliskardaras@gmail.com>
2025-06-20 15:47:56 +0300

    first commit

---
+++ src/main/java/com/vasiliskardaras/SpringSocialOauth2Demo/SpringSocialOauth2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.vasiliskardaras.SpringSocialOauth2Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialOauth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialOauth2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasiliskardaras/SpringSocialOauth2Demo/SpringSocialOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasiliskardaras.SpringSocialOauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialOauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
